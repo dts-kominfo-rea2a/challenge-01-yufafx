3bdc4d5edc6847995e0ef70ac48315b9a5447b6f
Yusuf Pratomo <yusufpra91@gmail.com>
2022-05-26 11:22:16 +0700

    feat:revisi struktur data set index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const firstUser = {
     gender: "Female",
     age: 17,
     email: "monica@dingdong.com",
-    favoriteColor: new Set(["Yellow", "Pink", "White", "Purple"]),
+    favoriteColor: (["Yellow", "Pink", "White", "Purple"]),
     isHavePet: "Yes",
     education: [
         {name: "SD 01", city: "Jakarta", graduate: 2016},
         {name: "SMP 02", city: "Jakarta", graduate: 2019},
         {name: "SMA 03", city: "Tangerang"}
     ],
-    favoriteRestaurant: new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"])
+    favoriteRestaurant: (["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"])
 };
 const secondUser = {
     name: "Wendy",
     gender: "Male",
     age: 23,
     email: "wendy@dingdong.com",
-    favoriteColor: new Set(["Blue", "Black", "Grey"]),
+    favoriteColor: (["Blue", "Black", "Grey"]),
     isHavePet: "No",
     education: [
         {name: "SD 02", city: "Jakarta", graduate: 2010},
@@ -28,13 +28,11 @@ const secondUser = {
         {name: "SMA 01", city: "Surabaya", graduate: 2016},
         {name: "Universitas Maju", city: "Tangerang"}
     ],
-    favoriteRestaurant: new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"])
+    favoriteRestaurant: (["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"])
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
-users.push(firstUser);
-users.push(secondUser);
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
